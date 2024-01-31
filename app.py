426e9403bd32a7492a97a321024eb1c835f4b4e1
dostonradjabov <radjabovdoston0809@gmail.com>
2024-01-31 09:50:06 -0500

    new4

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 import telegram
 # Create the application instance
-
 app = Flask(__name__)
+
 TOKEN ='6765226621:AAExg4YCtHJPi3vwJcZdLtAGIBRKGGU9YxU'
 bot = telegram.Bot(TOKEN)
 chat_id = '5377837814'
 
 # route for index page
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
-    bot.send_message(chat_id=chat_id, text='Hello World!!!')
+    print('index page')
+    #bot.send_message(chat_id=chat_id, text='Word')
     return 'index page'
